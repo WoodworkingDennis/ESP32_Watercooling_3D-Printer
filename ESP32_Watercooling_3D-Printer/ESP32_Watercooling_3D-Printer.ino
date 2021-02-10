7f757b211eb639645a9811d84b2c795af48d4bba
Dennis Wagner <dennis24.01@web.de>
2021-02-10 21:46:59 +0100

    Update ESP32_Watercooling_3D-Printer.ino

--- ESP32_Watercooling_3D-Printer/ESP32_Watercooling_3D-Printer.ino
+++ ESP32_Watercooling_3D-Printer/ESP32_Watercooling_3D-Printer.ino
@@ -1,15 +1,17 @@
 // Wasserkuehlung 3D-Drucker by @woodworking.dennis
 // Feb. 2021
 
+#include <MCP3XXX.h>
 
-#define DOOR          2
-#define LIGHT         32
+#define RELAY         32
 
 #define SW            4
 #define A             5
 #define B             13
 
 #define TOUCH         14
+
+#define CSMCP         2
 #define CSTOUCH       15
 #define CSDISPLAY     16
 #define CSSD          17
@@ -37,9 +39,14 @@ int pwm1speed   =     0;
 int pwm2speed   =     0;
 int pwm3speed   =     0;
 
+typedef MCP3XXX_<10, 8, 50000> MCP3208;
+MCP3008 mcp3208;
 
 void setup() {
   Serial.begin(115200);
+  
+  mcp3208.begin(CSMCP, MOSI, MISO, CLK);
+  
   Serial.println("ESP32 started");
 
   ledcSetup(PWM1CHAN, PWMFREQ, PWMRES);
@@ -54,9 +61,32 @@ void setup() {
   ledcAttachPin(PWM3, PWM3CHAN);
   ledcWrite(PWM3CHAN, pwm3speed);
 
+  pinMode(RELAY, OUTPUT);
+  digitalWrite(RELAY, LOW);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  Serial.println(mcp3208.analogRead(0));
+  Serial.println("-----");
+/*  Serial.println(mcp3208.analogRead(1));
+  Serial.println("-----");  
+  Serial.println(mcp3208.analogRead(2));
+  Serial.println("-----");
+  Serial.println(mcp3208.analogRead(3));
+  Serial.println("-----"); 
+  Serial.println(mcp3208.analogRead(4));
+  Serial.println("-----");
+  Serial.println(mcp3208.analogRead(5));
+  Serial.println("-----");
+  Serial.println(mcp3208.analogRead(6));
+  Serial.println("-----");
+  Serial.println(mcp3208.analogRead(7));
+  Serial.println("-----");
+  Serial.println("-----");
+  Serial.println("-----");
+*/
+  delay(500);
 
 }
